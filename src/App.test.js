cf67ce463a02fd1e8d3300534dfeb72a496b7281
Adarsh Kumar <adarsh010c@gmail.com>
2023-08-23 14:01:21 +0530

    Added button with style red.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('button has orrect intial color',() => {
+  render(<App/>)
+  //rab the button by text 
+  const colorButton = screen.getByRole('button', { name : 'Change to blue'})
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+})
+
+
+test('button color turns blue when clicked', () => {
+
+})
