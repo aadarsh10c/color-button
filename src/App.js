02efac8ca84554f6b1113f8f0ea4fe1bbd94212a
Adarsh Kumar <adarsh010c@gmail.com>
2023-08-23 15:32:27 +0530

    Tested and coded button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [change, setChange] = React.useState(false)
   return (
     <div className='App'>
-      <button style={{backgroundColor: 'red'}} >Change to blue</button>
+      {
+        change ? (
+          <button 
+            onClick={() => setChange(prevValue => !prevValue)} 
+            style={{backgroundColor: 'blue'}} >Change to red</button>
+        ):(
+          <button 
+            onClick={() => setChange(prevValue => !prevValue)} 
+            style={{backgroundColor: 'red'}} >Change to blue</button>
+
+        )
+      }
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('button has orrect intial color',() => {
+test('button has correct intial color',() => {
   render(<App/>)
   //rab the button by text 
   const colorButton = screen.getByRole('button', { name : 'Change to blue'})
@@ -10,5 +10,19 @@ test('button has orrect intial color',() => {
 
 
 test('button color turns blue when clicked', () => {
+  render(<App/>)
 
+  //Gra the olor button
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+
+  //click event
+  fireEvent.click(colorButton)
+
+  //expect to change the color
+  expect(colorButton).toHaveStyle(`
+    background-color: blue;
+  `)
+
+  //epect to have update the text
+  expect(colorButton).toHaveTextContent('Change to red')
 })
